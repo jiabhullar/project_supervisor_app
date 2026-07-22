77dc392d7dcf8f3c2f2ec46f1d5c981a4398b142
Jia Bhullar <jiakbhullar@gmail.com>
2026-07-22 15:01:55 +0100

    add project idea data to model file

---
+++ lib/models/project_idea.dart
@@ -0,0 +1,13 @@
+class ProjectIdea {
+  final String id;
+  final String staffId;
+  final String title;
+  final String researchArea;
+
+  ProjectIdea({
+    required this.id,
+    required this.staffId,
+    required this.title,
+    required this.researchArea,
+  });
+}
