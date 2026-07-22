18c4f2c90528d3f5c2cb861bb744fb2943b3e403
Jia Bhullar <jiakbhullar@gmail.com>
2026-07-22 15:02:23 +0100

    add description element

--- lib/models/project_idea.dart
+++ lib/models/project_idea.dart
@@ -2,12 +2,14 @@ class ProjectIdea {
   final String id;
   final String staffId;
   final String title;
+  final String description;
   final String researchArea;
 
   ProjectIdea({
     required this.id,
     required this.staffId,
     required this.title,
+    required this.description,
     required this.researchArea,
   });
 }
