dd39fc956583b331fe847f59597ba2239a1d5680
Jia Bhullar <jiakbhullar@gmail.com>
2026-07-22 19:11:41 +0100

    add staff login form validation

--- lib/pages/staff_login.dart
+++ lib/pages/staff_login.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
 
-class StaffLoginScreen extends StatelessWidget {
+class StaffLoginScreen extends StatefulWidget {
   const StaffLoginScreen({super.key});
 
+  @override
+  State<StaffLoginScreen> createState() => _StaffLoginScreenState();
+}
+
+class _StaffLoginScreenState extends State<StaffLoginScreen> {
+  String email = '';
+  String password = '';
+
+  void signIn() {
+    if (email.isEmpty || password.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Please enter both email and password.'),
+        ),
+      );
+      return;
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Login details entered successfully.'),
+        ),
+      );
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,26 +55,31 @@ class StaffLoginScreen extends StatelessWidget {
                       style: Theme.of(context).textTheme.titleLarge,
                     ),
                     const SizedBox(height: 20),
-                    const TextField(
-                      decoration: InputDecoration(
+                    TextField(
+                      decoration: const InputDecoration(
                         labelText: 'Email address',
                         prefixIcon: Icon(Icons.email),
                         border: OutlineInputBorder(),
                       ),
+                      onChanged: (String value) {
+                        email = value.trim();
+                      },
                     ),
                     const SizedBox(height: 15),
-                    const TextField(
+                    TextField(
                       obscureText: true,
-                      decoration: InputDecoration(
+                      decoration: const InputDecoration(
                         labelText: 'Password',
                         prefixIcon: Icon(Icons.lock),
                         border: OutlineInputBorder(),
                       ),
+                      onChanged: (String value) {
+                        password = value;
+                      },
                     ),
                     const SizedBox(height: 20),
                     ElevatedButton(
-                      onPressed: () {
-                      },
+                      onPressed: signIn,
                       child: const Text('Sign In'),
                     ),
                   ],
